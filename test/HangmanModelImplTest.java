53d7ba1dc3a63cbb1328e7333555fa84703f7474
sillyfunnypedro <sillyfunnypedro@gmail.com>
2024-03-11 10:18:10 -0700

    building the git repo

--- test/HangmanModelImplTest.java
+++ test/HangmanModelImplTest.java
@@ -1,9 +1,18 @@
+import hangman.HangmanModel;
+import hangman.HangmanModelImpl;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
 
 public class HangmanModelImplTest {
+
+  private HangmanModel game;
+
+  @Before
+  public void setUp() {
+    game = new HangmanModelImpl("res/test.txt");
+  }
 
   @Test
   public void aTestThatPasses() {
