d8df117edaf039d7ee1bec5eaa0ea30614b70c51
sillyfunnypedro <sillyfunnypedro@gmail.com>
2024-03-11 06:34:28 -0700

    building the git repo

---
+++ test/HangmanModelImplTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+
+class HangmanModelImplTest {
+
+  @Test
+  void initialize() {
+  }
+
+  @Test
+  void makeGuess() {
+  }
+
+  @Test
+  void isGameOver() {
+  }
+
+  @Test
+  void getCurrentState() {
+  }
+
+  @Test
+  void getGuessesRemaining() {
+  }
+}
